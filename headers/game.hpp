bd9bf6c1aa5e1b4c190ca7bd589588ac2759061d
rida-fatima03 <89501946+rida-fatima03@users.noreply.github.com>
2022-11-19 12:52:41 +0500

    Update game.hpp

---
+++ headers/game.hpp
@@ -0,0 +1,31 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+class Game{
+    //Screen dimension constants
+    const int SCREEN_WIDTH = 680;
+    const int SCREEN_HEIGHT = 480;
+
+    //The window we'll be rendering to
+    SDL_Window* gWindow = NULL;
+
+    //Current displayed texture
+    SDL_Texture* gTexture = NULL;
+    
+
+public:
+
+
+
+    bool init();
+    bool loadMedia();
+    void close();
+    SDL_Texture* loadTexture( std::string path );
+    void run();
+};
+
